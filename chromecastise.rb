88741e047a01787164b6d277a3f542f0622eab3d
Stuart Moss <stuart.moss@helpsystems.com>
2021-09-03 20:33:10 +0100

    feat: update chromecastise to v1.0.8

--- chromecastise.rb
+++ chromecastise.rb
@@ -1,14 +1,13 @@
 class Chromecastise < Formula
   desc ""
   homepage "https://github.com/stumoss/chromecastise"
-  url "https://github.com/stumoss/chromecastise/archive/1.0.0.tar.gz"
-  sha256 "aa6b376db39bbc9f89f6c128fe836ded16847c0a74ef586fa908e607762371e8"
+  url "https://github.com/stumoss/chromecastise/archive/refs/tags/v1.0.8.tar.gz"
+  sha256 "1875dda04b966d79b0433b77a7b88d330c4d63a35f91a00b8f7b59beec3d4905"
 
   head "https://github.com/stumoss/chromecastise.git", :branch => "develop"
 
   depends_on "rust" => :build
   depends_on "ffmpeg" => [:run, "with-fdk-aac", "with-sdl2", "with-libass", "with-libvorbis", "with-libvpx", "with-opus", "with-x265"]
-  depends_on "ffmpeg" => :run
   depends_on "media-info" => :run
 
   def install
